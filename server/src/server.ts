be137806fcfc57a5e34992a7d49ac2e63f85d729
Miles Felton Gerber <mwfelton03@gmail.com>
2024-12-03 20:46:54 +0000

    time endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
   });
+
+  app.get('/time', (req, res) => {
+    const serverTime = {
+      epoch: Math.floor(Date.now() / 1000),
+    };
+    res.json(serverTime);
+  });
   
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
