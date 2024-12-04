2ec4190536f2afdb2d074e192c1f5d12add0e889
Miles Felton Gerber <mwfelton03@gmail.com>
2024-12-04 20:00:04 +0000

    base level styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import TimeComponent from '../components/Time/Time';
 import MetricsComponent from '../components/Metrics/Metrics';
+import './Home.css'; // Import the updated CSS
 
 const HomePage: React.FC = () => {
     return (
-        <div>
-            <h1>Welcome to the Dashboard</h1>
-            <TimeComponent />
-            <MetricsComponent />
+        <div className="HomePage">
+            {/* Left Section */}
+            <div id="left">
+                <h1>Time</h1>
+                <TimeComponent />
+            </div>
+
+            {/* Right Section */}
+            <div id="right">
+                <h1>Metrics</h1>
+                <MetricsComponent />
+            </div>
         </div>
     );
 };
